7ec1b92d7b9bd0429f8e7826da25d220873da0d8
gardeniacity <149237733+gardeniacity@users.noreply.github.com>
2023-12-02 19:53:27 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,59 @@
+// Service Worker
+
+// Cache name
+const CACHE_NAME = 'my-site-cache-v1';
+
+// Files to cache
+const urlsToCache = [
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/data.js'
+];
+
+// Install the service worker
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetching content
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // Clone the request
+        const fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone the response
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+  );
+});
